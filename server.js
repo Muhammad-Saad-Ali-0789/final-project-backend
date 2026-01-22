897562d5551f169ec70070c5efa53487eeb671a2
Saadali198 <muhammadsaadakbar.386@gmail.com>
2026-01-22 12:54:20 +0500

    error fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://final-project-frontend-plum.vercel.app'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
